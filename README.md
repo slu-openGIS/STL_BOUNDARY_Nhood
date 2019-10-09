616a6f0782720ee0a79820c90d41c1836aa40dfc
Christopher Prener <chris.prener@slu.edu>
2019-10-08 19:20:30 -0500

    add geojson files

---
+++ README.md
@@ -0,0 +1,2 @@
+# STL_BOUNDARY_Nhood
+This repository contains individual `.geojson` files in the `data/` folder for each of the 88 neighborhoods in the City of St. Louis. Source code for creating these can be found in `source/`.
---
+++ source/parse_nhood.R
@@ -0,0 +1,40 @@
+# dependencies
+library(dplyr)     # data wrangling
+library(gateway)   # st. louis data
+library(here)      # file path management
+library(purrr)     # iteration
+library(sf)        # spatial tools
+library(stringr)   # string manipulation
+
+# prepare data
+## download neighborhood boundaries
+nhood <- gw_get_data(data = "Neighborhoods", class = "sf")
+
+## re-project to WGS 1984
+nhood <- st_transform(nhood, crs = 4326)
+
+## remove columns
+nhood <- select(nhood, NHD_NUM, NHD_NAME)
+
+## store vector of neighborhood IDs
+ids <- nhood$NHD_NUM
+
+# define function for subsetting and writing single neighborhood files
+parse_nhood <- function(.data, id){
+  
+  # subset to identified neighborhood
+  working <- filter(.data, NHD_NUM == id)
+  
+  # create filename
+  filename <- str_replace_all(working$NHD_NAME[1], pattern = "[\\s-/]", replacement = "_")
+  filename <- paste0(filename, ".geojson")
+  
+  # write to data
+  st_write(working, dsn = here("data", filename), driver = "GeoJSON", delete_dsn = TRUE)
+  
+}
+
+# save all neighborhoods individually
+ids %>%
+  unlist() %>%
+  map(~ parse_nhood(nhood, id = .x))
